94a50ca5dc13fef7bb7b57cedaaa7a2453b076a1
Neko <22877223+NekoC1@users.noreply.github.com>
2024-01-18 04:01:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ exit 1
 function install() {
     cd /tmp/
     send_msg "downloading full.bin"
-    wget -N --no-check-certificate -O full.bin https://github.com/DoubiNeko/catdrive-syno/raw/master/full.bin
+    wget -N --no-check-certificate -O full.bin https://github.com/NekoC1/catdrive-syno/raw/master/full.bin
     on_error_exit "download full.bin failed"
     
     send_msg "start writing mdt0"
